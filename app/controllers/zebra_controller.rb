95bb651a574332bf301ed0131bbb4591280dda42
Chuxiness <146590179+Chuxiness@users.noreply.github.com>
2023-11-25 05:34:29 +0000

    Functional app

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -9,4 +9,28 @@ def giraffe
   end
   render ({:template=>"game_templates/play_rock"})
 end
+
+def elephant 
+  @comp_move = ["rock","paper","scissors"].sample
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else @outcome = "lost"
+  end
+  render ({:template=>"game_templates/play_paper"})
+end
+
+def lion 
+  @comp_move = ["rock","paper","scissors"].sample
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else @outcome = "tied"
+  end
+  render ({:template=>"game_templates/play_scissors"})
+end
+
+
 end
